e0077e7890f9b3b6d57b4164fcfa966f35a3a0a8
Tettei Shahday-Annang <tetteis@gmail.com>
2022-07-12 07:11:04 +0000

    Projects section complete

---
+++ components/Projects.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import ProjectItem from "./ProjectItem";
+import propertyImg from "../public/assets/projects/property.jpg";
+import cryptoImg from "../public/assets/projects/crypto.jpg";
+import netflixImg from "../public/assets/projects/netflix.jpg";
+import twitchImg from "../public/assets/projects/twitch.jpg";
+
+function Projects() {
+  return (
+    <div className="w-full">
+      <div className="max-w-[1240px] mx-auto px-2 py-16">
+        <p className="text-xl track-widest uppercase text-[#5651e5]">
+          Projects
+        </p>
+        <h2 className="py-4">What I&apos;ve Built</h2>
+        <div className="grid md:grid-cols-2 gap-8">
+          <ProjectItem
+            title="Property Finder"
+            backgroundImg={propertyImg}
+            projectUrl="/property"
+          />
+          <ProjectItem
+            title="Crypto App"
+            backgroundImg={cryptoImg}
+            projectUrl="/crypto"
+          />
+          <ProjectItem
+            title="Netflix Clone"
+            backgroundImg={netflixImg}
+            projectUrl="/netflix"
+          />
+          <ProjectItem
+            title="Twitch Clone"
+            backgroundImg={twitchImg}
+            projectUrl="/twitch"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Projects;
---
+++ components/Skills.jsx
@@ -0,0 +1,146 @@
+import Image from "next/image";
+import React from "react";
+
+function Skills() {
+  return (
+    <div className="w-full lg:h-screen p-2">
+      <div className="max-w-[1240] mx-auto flex flex-col justify-center h-full">
+        <p className="text-xl tracking-widest uppercase text-[#5651e5]">
+          Skills
+        </p>
+        <h2 className="py-4">What I Can Do</h2>
+        <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src="/assets/skills/tailwind.svg"
+                  alt="Tailwind CSS"
+                  width="64px"
+                  height="64px"
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Tailwind CSS</h3>
+              </div>
+            </div>
+          </div>
+
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src="/assets/skills/typescript.svg"
+                  alt="TypeScript"
+                  width="64px"
+                  height="64px"
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>TypeScript</h3>
+              </div>
+            </div>
+          </div>
+
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src="/assets/skills/react.svg"
+                  alt="React"
+                  width="64px"
+                  height="64px"
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>React</h3>
+              </div>
+            </div>
+          </div>
+
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src="/assets/skills/graphql.svg"
+                  alt="GraphQL"
+                  width="64px"
+                  height="64px"
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>GraphQL</h3>
+              </div>
+            </div>
+          </div>
+
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src="/assets/skills/firebase.svg"
+                  alt="Firebase"
+                  width="64px"
+                  height="64px"
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Firebase</h3>
+              </div>
+            </div>
+          </div>
+
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src="/assets/skills/strapi.svg"
+                  alt="Strapi"
+                  width="64px"
+                  height="64px"
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Strapi</h3>
+              </div>
+            </div>
+          </div>
+
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src="/assets/skills/aws.svg"
+                  alt="Amazon Web Services Amplify"
+                  width="64px"
+                  height="64px"
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>AWS Amplify</h3>
+              </div>
+            </div>
+          </div>
+
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src="/assets/skills/github.svg"
+                  alt="GitHub"
+                  width="64px"
+                  height="64px"
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>GitHub</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Skills;
